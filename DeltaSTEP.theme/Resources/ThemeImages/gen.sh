f0d9b79b0fc50f634900678751229732b4c33a7e
Tim Sheridan <tghs@tghs.net>
2015-12-12 23:16:01 +0000

    Add download directory icon

--- DeltaSTEP.theme/Resources/ThemeImages/gen.sh
+++ DeltaSTEP.theme/Resources/ThemeImages/gen.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+convert common_DownloadFolder_48.png -adjoin common_DownloadFolder.tiff
 convert common_Folder_48.png -adjoin common_Folder.tiff
 convert common_HomeDirectory_48.png -adjoin common_HomeDirectory.tiff
