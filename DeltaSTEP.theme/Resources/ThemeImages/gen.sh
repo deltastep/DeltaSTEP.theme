eb1631dbce0e1bcb7a99f8d8fd6f30e43f89e296
Tim Sheridan <tghs@tghs.net>
2015-12-12 23:39:13 +0000

    Add computer icon

--- DeltaSTEP.theme/Resources/ThemeImages/gen.sh
+++ DeltaSTEP.theme/Resources/ThemeImages/gen.sh
@@ -3,3 +3,4 @@
 convert common_DownloadFolder_48.png -adjoin common_DownloadFolder.tiff
 convert common_Folder_48.png -adjoin common_Folder.tiff
 convert common_HomeDirectory_48.png -adjoin common_HomeDirectory.tiff
+convert common_Root_PC_48.png -adjoin common_Root_PC.tiff
